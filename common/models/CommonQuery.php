5b16a2220fb0a663b567dd451f08ab8f79c54d22
denoll <odanic@yandex.ru>
2016-02-03 06:39:34 +0300

    yandex kassa

---
+++ common/models/CommonQuery.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by denoll.
+ * User: Администратор
+ * Date: 03.02.2016
+ * Time: 6:35
+ */
+
+namespace common\models;
+
+
+use yii\db\ActiveRecord;
+
+class CommonQuery extends ActiveRecord
+{
+	/**
+	 * Delete item from catalog.
+	 * @param string $item Item model
+	 * @param string $img_path
+	 * @return mixed
+	 */
+	public static function deleteItem($item, $img_path)
+	{
+		$dir = Yii::getAlias($img_path);
+		if(is_dir($dir)){
+			$images = FileHelper::findFiles($dir, [
+				'only' => [
+					$item->main_img,
+				],
+			]);
+			for ($n = 0; $n != count($images); $n++) {
+				@unlink($images[$n]);
+			}
+		}
+		//delete row from database
+		if ($item->delete()) {
+			Yii::$app->session->setFlash('success', 'Объявление успешно удалено.');
+		}
+	}
+}
